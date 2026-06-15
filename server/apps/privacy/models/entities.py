733bc46c8249b3ca8ece6aef1f8629f5754447f3
Neeschal1 <neeschalpok04@gmail.com>
2026-06-15 13:19:56 +0545

    ft: append all models related to users privacy

--- server/apps/privacy/models/entities.py
+++ server/apps/privacy/models/entities.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from apps.userprofile.models.entities import UserProfile
+from .choices import REPORT_REASONS
 
 
 class UserPrivacy(models.Model):
@@ -43,3 +44,39 @@ class FavouriteDetail(models.Model):
     def __str__(self):
         return f"{self.user_privacy} -> {self.favourites_user}"
     
+
+class FollowDetail(models.Model):
+    user_privacy = models.ForeignKey(UserPrivacy, on_delete=models.CASCADE, related_name='user_following_other')
+    following_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='followed_user') 
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user_privacy', 'following_user'],
+                name='unique_follow'
+            )
+        ]
+    
+    def __str__(self):
+        return f"{self.user_privacy} -> {self.following_user}"
+    
+
+class ReportUser(models.Model):
+    user_privacy = models.ForeignKey(UserPrivacy, on_delete=models.CASCADE, related_name='user_reporting_other')
+    reported_id = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='reported_user') 
+    reason = models.CharField(max_length=30, choices=REPORT_REASONS)
+    description = models.TextField(blank=True,   default="The reported user used abusive language or inappropriate behavior.")
+    status = models.CharField(max_length=30, default="Pending")
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user_privacy', 'reported_id'],
+                name='unique_report_data'
+            )
+        ]
+    
+    def __str__(self):
+        return f"{self.user_privacy} -> {self.reported_id}"
