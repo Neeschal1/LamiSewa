8350c79d969206a158831fb878e07f3f964f8162
Neeschal1 <neeschalpok04@gmail.com>
2026-06-19 23:14:21 +0545

    ft: implement interactive reanimation in welcome screen

--- client/src/screens/initials/WelcomeScreen.tsx
+++ client/src/screens/initials/WelcomeScreen.tsx
@@ -1,6 +1,5 @@
 import {
   View,
-  Text,
   ImageBackground,
   StatusBar,
   Image,
@@ -13,6 +12,16 @@ import {
   TextualButton,
 } from "@/src/components/systemComponentsLayout";
 import React, { FC } from "react";
+import Animated, {
+  FadeIn,
+  FadeOut,
+  FadeInUp,
+  FadeInDown,
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  BounceIn,
+} from "react-native-reanimated";
 
 const Welcomecontents = require("@/src/assets/images/welcomeBanner.png");
 const logo = require("@/src/assets/images/mainLogo.png");
@@ -21,7 +30,6 @@ const screenheight = Dimensions.get("window").height;
 const screenwidth = Dimensions.get("window").width;
 
 const Welcome: FC = () => {
-
   return (
     <View className="flex-1">
       <StatusBar hidden translucent />
@@ -30,17 +38,23 @@ const Welcome: FC = () => {
         source={Welcomecontents}
       >
         <View className="flex justify-center items-center mb-mid gap-small">
-          <Image
+          <Animated.View entering={FadeInUp.delay(400).duration(800).springify()}>
+            <Image
             style={{ width: screenwidth * 0.7, height: screenheight * 0.143 }}
             source={logo}
           />
-          <PrimaryButton screen="Signup" text="Let's Begin" />
-          <View className="flex flex-row gap-2 justify-center items-center">
+          </Animated.View>
+          <Animated.View entering={FadeInUp.delay(200).duration(600).springify()}>
+            <PrimaryButton screen="Signup" text="Let's Begin" />
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(200).duration(600).springify()}  className="flex flex-row gap-2 justify-center items-center">
             <SubTitle text="Already have an account?" />
             <TextualButton text="Login" screen="Login" />
-          </View>
+          </Animated.View>
         </View>
-        <Description text="LamiSewa © 2026. All rights reserved." />
+        <Animated.View entering={FadeInDown.delay(400).duration(800).springify()}>
+          <Description text="LamiSewa © 2026. All rights reserved." />
+        </Animated.View>
       </ImageBackground>
     </View>
   );
