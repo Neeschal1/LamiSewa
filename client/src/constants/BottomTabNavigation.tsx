edc371cf708458c8d72cdc489266a5fb41fdcfbc
Neeschal1 <neeschalpok04@gmail.com>
2026-06-23 14:24:54 +0545

    md: bottom tab bar modification for easy navigation between main screens

--- client/src/constants/BottomTabNavigation.tsx
+++ client/src/constants/BottomTabNavigation.tsx
@@ -6,6 +6,8 @@ import {
   Profile,
   Search,
 } from "@/src/screens/main/mainScreenLayouts";
+import { View, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
@@ -13,13 +15,160 @@ const MyTabs = () => {
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      screenOptions={{ headerShown: false }}
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          height: 70,
+          backgroundColor: "#F2F1FF",
+          borderTopWidth: 0,
+          paddingBottom: 10,
+          paddingTop: 15,
+          alignItems: 'center',
+          justifyContent: 'center'
+        },
+      }}
     >
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Notification" component={Notification} />
-      <Tab.Screen name="Search" component={Search} />
-      <Tab.Screen name="Chats" component={Chats} />
-      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                width: 70,
+              }}
+            >
+              <Ionicons
+                name={focused ? "home" : "home-outline"}
+                size={24}
+                color={focused ? "#FF000E" : "#7B7B7B"}
+              />
+              {focused ? (
+                <Text
+                  className={`font-Poppinsregular text-[12px] ${focused ? "text-primaryred" : "text-darkvariant"}`}
+                >
+                  Home
+                </Text>
+              ) : null}
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notification"
+        component={Notification}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                width: 80,
+              }}
+            >
+              <Ionicons
+                name={focused ? "notifications" : "notifications-outline"}
+                size={24}
+                color={focused ? "#FF000E" : "#7B7B7B"}
+              />
+              {focused ? (
+                <Text
+                  className={`font-Poppinsregular text-[12px] ${focused ? "text-primaryred" : "text-darkvariant"}`}
+                >
+                  Notification
+                </Text>
+              ) : null}
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                width: 50,
+              }}
+            >
+              <Ionicons
+                name={focused ? "search" : "search-outline"}
+                size={24}
+                color={focused ? "#FF000E" : "#7B7B7B"}
+              />
+              {focused ? (
+                <Text
+                  className={`font-Poppinsregular text-[12px] ${focused ? "text-primaryred" : "text-darkvariant"}`}
+                >
+                  Search
+                </Text>
+              ) : null}
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Chats"
+        component={Chats}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                width: 40,
+              }}
+            >
+              <Ionicons
+                name={focused ? "chatbox" : "chatbox-outline"}
+                size={24}
+                color={focused ? "#FF000E" : "#7B7B7B"}
+              />
+              {focused ? (
+                <Text
+                  className={`font-Poppinsregular text-[12px] ${focused ? "text-primaryred" : "text-darkvariant"}`}
+                >
+                  Chats
+                </Text>
+              ) : null}
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                width: 50,
+              }}
+            >
+              <Ionicons
+                name={focused ? "person" : "person-outline"}
+                size={24}
+                color={focused ? "#FF000E" : "#7B7B7B"}
+              />
+              {focused ? (
+                <Text
+                  className={`font-Poppinsregular text-[12px] ${focused ? "text-primaryred" : "text-darkvariant"}`}
+                >
+                  Profile
+                </Text>
+              ) : null}
+            </View>
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
