61c9a375715f9e66a6275f38058443fe3f2cdaf9
Neeschal1 <neeschalpok04@gmail.com>
2026-07-19 09:22:20 +0545

    ft: implementation of custom admin panel with css and js integration

--- server/static/charts.js
+++ server/static/charts.js
@@ -402,4 +402,60 @@ document.addEventListener("DOMContentLoaded", function () {
 
     verificationChart.render();
   }
+
+  const ageElement = document.querySelector("#ageChart");
+
+  if (ageElement) {
+    const ageChart = new ApexCharts(ageElement, {
+      chart: {
+        type: "bar",
+        height: 320,
+        toolbar: {
+          show: false,
+        },
+      },
+
+      series: [
+        {
+          name: "Users",
+          data: [3542, 5291, 2175, 864],
+        },
+      ],
+
+      colors: ["#6366F1"],
+
+      plotOptions: {
+        bar: {
+          horizontal: true,
+
+          borderRadius: 8,
+
+          barHeight: "45%",
+        },
+      },
+
+      dataLabels: {
+        enabled: false,
+      },
+
+      grid: {
+        borderColor: "#E5E7EB",
+        strokeDashArray: 5,
+      },
+
+      xaxis: {
+        categories: ["18 - 24", "25 - 30", "31 - 35", "36+"],
+      },
+
+      tooltip: {
+        y: {
+          formatter: function (val) {
+            return val.toLocaleString() + " Users";
+          },
+        },
+      },
+    });
+
+    ageChart.render();
+  }
 });
